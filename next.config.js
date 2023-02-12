0d2f63ae227ab92fa991c8d997b406877996ef03
ShivangM <shivangmishra0824@gmail.com>
2023-02-12 17:28:07 +0530

    TS Ignore

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
       'source.unsplash.com',
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
